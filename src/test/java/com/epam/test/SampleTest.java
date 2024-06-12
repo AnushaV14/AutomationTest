e027fee653b698a6288854b816a47c2bd16a64e5
Anusha Vujjini <anusha_vujjini@epam.com>
2024-06-12 14:28:36 +0530

    SampleTest class modified

--- src/test/java/com/epam/test/SampleTest.java
+++ src/test/java/com/epam/test/SampleTest.java
@@ -5,6 +5,14 @@ public class SampleTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Sample Test Added");
+		m1(5, 6);
+	}
+	
+	public static int m1(int a, int b) {
+		int c = a+b;
+		System.out.println("a , b values are "+a +" ," + b);
+		System.out.println("result is: "+c);
+		return c;
 	}
 
 }
